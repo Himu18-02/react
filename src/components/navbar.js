53e0f07853d7da79b65dbdaf6210ee8fc3e4ec98
Himanshu Singh <himu.delhi2015@gmail.com>
2021-10-27 18:12:49 +0530

    third

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,14 +7,14 @@ export default class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand text-info" href="/">HIMU</a>
+                        <Link className="navbar-brand text-info" to="/">HIMU</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                                 </li>
                                
                             </ul>
